05f1a859118e3bcef6928e0660b12cd5722ed641
younusraza909 <younusraza909@gmail.com>
2023-06-22 13:29:44 +0500

    Filter method in array js

--- js-review/array-method.js
+++ js-review/array-method.js
@@ -8,3 +8,15 @@ const x = [1, 2, 3, 4, 5].map((el) => el * 2);
 console.log(x);
 
 const titles = books.map((book) => book.title);
+
+// Filter Mehod
+
+const longBooksWithMovie = books
+  .filter((book) => book.pages > 500)
+  .filter((book) => book.hasMovieAdaptation);
+longBooksWithMovie;
+
+const adventureBooks = books
+  .filter((books) => books.genres.includes("adventure"))
+  .map((book) => book.title);
+adventureBooks;
