f2a2bfdab6923139d881469cfcea04b8e65b793c
younusraza909 <younusraza909@gmail.com>
2023-06-22 14:46:45 +0500

    Working with immutable aray without touching underlying structure

--- js-review/array-method.js
+++ js-review/array-method.js
@@ -26,6 +26,8 @@ pagesAllBooks;
 
 // Sort Method
 
+//using slice we prevent orgianl array to mutate
+
 const arr = [3, 7, 1, 9, 6];
 const sorted = arr.slice().sort((a, b) => a - b);
 sorted;
@@ -33,3 +35,25 @@ arr;
 
 const sortedByPages = books.slice().sort((a, b) => a.pages - b.pages);
 sortedByPages;
+
+// Working with immutable array
+
+// 1) Add a book object to array
+const newBook = {
+  id: 6,
+  title: "Harry Potter and the Chamber of Secrets",
+  author: "J. K. Rowling",
+};
+
+const booksAfterAdd = [...books, newBook];
+booksAfterAdd;
+
+// 2) Delete book object from array
+const booksAfterDelete = booksAfterAdd.filter((book) => book.id !== 3);
+booksAfterDelete;
+
+// 3) Update book object in the array
+const booksAfterUpdate = booksAfterDelete.map((book) =>
+  book.id === 1 ? { ...book, pages: 1210 } : book
+);
+booksAfterUpdate;
