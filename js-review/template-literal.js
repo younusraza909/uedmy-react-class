2eeaa89d6d9e8674c688ec1aa31f3d542307814c
younusraza909 <younusraza909@gmail.com>
2023-06-22 12:25:34 +0500

    bug in template-literal

--- js-review/template-literal.js
+++ js-review/template-literal.js
@@ -4,6 +4,6 @@ let book = getBook(1);
 
 const { title, author, pages, publicationDate } = book;
 
-const summary = `${title}, a ${pages}-page long book, was written by ${author} and published in ${getYear(
-  publicationDate
-)}. The book has ${hasMovieAdaptation ? "" : "not"} been adapted as a movie`;
+const summary = `${title}, a ${pages}-page long book, was written by ${author} and published in ${publicationDate}. The book has ${
+  hasMovieAdaptation ? "" : "not"
+} been adapted as a movie`;
