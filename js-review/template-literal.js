bf0cef957b69d203f67cfe7f64a51ede787a1611
younusraza909 <younusraza909@gmail.com>
2023-06-22 12:18:17 +0500

    Js String template literal

---
+++ js-review/template-literal.js
@@ -0,0 +1,9 @@
+import { getBook } from "./data";
+
+let book = getBook(1);
+
+const { title, author, pages, publicationDate } = book;
+
+const summary = `${title}, a ${pages}-page long book, was written by ${author} and published in ${getYear(
+  publicationDate
+)}. The book has ${hasMovieAdaptation ? "" : "not"} been adapted as a movie`;
