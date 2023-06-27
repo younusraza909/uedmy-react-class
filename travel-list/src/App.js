37a3788f8efd0563338f944d3876af1a0d0d8427
younusraza909 <younusraza909@gmail.com>
2023-06-27 04:31:25 -0700

    Rendering list

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
@@ -12,13 +17,32 @@ function Form() {
   );
 }
 
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
 function PackagingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>💼 You have X on your list, and you already packed X (X%)</em>
     </footer>
   );
@@ -26,12 +50,12 @@ function Stats() {
 
 const App = () => {
   return (
-    <>
+    <div className="app">
       <Logo />
       <Form />
       <PackagingList />
       <Stats />
-    </>
+    </div>
   );
 };
 
