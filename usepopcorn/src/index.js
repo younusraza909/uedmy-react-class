7ee3a99987ef67f1dc7556845e4e0f9fbd34f848
younusraza909 <younusraza909@gmail.com>
2023-07-12 14:16:01 +0500

    Star Component created in isolated

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import StartRating from "./StartRating";
+// import App from "./App";
+// import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StartRating maxRating={5} />
   </React.StrictMode>
 );
