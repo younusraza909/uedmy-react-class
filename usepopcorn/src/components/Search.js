49a5d9e13a3103e66c51dcace8bcfdccd4e706d1
younusraza909 <younusraza909@gmail.com>
2023-08-07 10:30:17 +0500

    useRef hook and add event listner

--- usepopcorn/src/components/Search.js
+++ usepopcorn/src/components/Search.js
@@ -1,6 +1,28 @@
+import { useEffect, useRef } from "react";
+
 function Search({ query, setQuery }) {
+  const inputElement = useRef(null);
+
+  useEffect(() => {
+    function callback(e) {
+      if (e.code === "Enter") {
+        // now we dont want this functionality if currently our input field is active
+        // to check it
+        if (document.activeElement === inputElement.current) return;
+
+        inputElement.current.focus();
+        setQuery("");
+      }
+    }
+
+    document.addEventListener("keydown", callback);
+
+    return () => document.removeEventListener("keydown", callback);
+  }, [setQuery]);
+
   return (
     <input
+      ref={inputElement}
       className='search'
       type='text'
       placeholder='Search movies...'
