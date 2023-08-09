93a254c941236bd1eac80b944ddf8033ecc2c4cc
younusraza909 <younusraza909@gmail.com>
2023-08-09 16:55:02 +0500

    Removed all setState and used useReducer instead

--- react-quiz/src/DateCounter.js
+++ react-quiz/src/DateCounter.js
@@ -1,14 +1,32 @@
 import { useReducer, useState } from "react";
 
+const intialState = {
+  count: 0,
+  step: 1,
+};
+
 function reducer(state, action) {
-  if (action.type === "inc") return state + 1;
-  if (action.type === "dec") return state - 1;
-  if (action.type === "setCount") return action.payload;
+  switch (action.type) {
+    case "dec":
+      return { ...state, count: state.count - state.step };
+    case "inc":
+      return { ...state, count: state.count + state.step };
+    case "setCount":
+      return { ...state, count: action.payload };
+
+    case "setStep":
+      return { ...state, step: action.payload };
+    case "reset":
+      return { count: 0, step: 1 };
+    default:
+      break;
+  }
 }
 
 function DateCounter() {
-  const [count, dispatch] = useReducer(reducer, 0);
-  const [step, setStep] = useState(1);
+  const [state, dispatch] = useReducer(reducer, intialState);
+
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
@@ -32,12 +50,12 @@ function DateCounter() {
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    dispatch({ type: "setStep", payload: Number(e.target.value) });
   };
 
   const reset = function () {
     // setCount(0);
-    setStep(1);
+    dispatch({ type: "reset" });
   };
 
   return (
