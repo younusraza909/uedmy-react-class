8f64589b00819028fb44e9f8c2dfbd5c8c382762
younusraza909 <younusraza909@gmail.com>
2023-08-10 11:12:22 +0500

    Moving to new Question

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -5,6 +5,7 @@ import Loader from "./components/Loader";
 import Error from "./components/Error";
 import StartScreen from "./components/StartScreen";
 import Question from "./components/Question";
+import NextButton from "./components/NextButton";
 
 const initialState = {
   questions: [],
@@ -33,6 +34,8 @@ function reducer(state, action) {
             ? state.points + question.points
             : state.points,
       };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
     default:
       throw new Error("Action unknown");
   }
@@ -66,11 +69,14 @@ function App() {
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
         {status === "active" && (
-          <Question
-            question={questions[index]}
-            dispatch={dispatch}
-            answer={answer}
-          />
+          <>
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
         )}
       </Main>
     </div>
---
+++ react-quiz/src/components/NextButton.js
@@ -0,0 +1,12 @@
+function NextButton({ dispatch, answer }) {
+  return (
+    <button
+      className='btn btn-ui'
+      onClick={() => dispatch({ type: "nextQuestion" })}
+    >
+      Next
+    </button>
+  );
+}
+
+export default NextButton;
