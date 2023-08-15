e7bb7066e3006c0bd3e4374e110312c619bd49cf
younusraza909 <younusraza909@gmail.com>
2023-08-15 11:59:51 -0700

    Static App Lyout

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -4,13 +4,13 @@ import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
-// import AppLayout from "./pages/AppLayout";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="app" element={<AppLayout/>}/> */}
+        <Route path="app" element={<AppLayout />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
--- worldwise/src/components/AppNav.jsx
+++ worldwise/src/components/AppNav.jsx
@@ -1,5 +1,19 @@
-function AppNav(){
-    return <div>App navigation</div>
+import { NavLink } from "react-router-dom";
+import styles from "./AppNav.module.css";
+
+function AppNav() {
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>
+        </li>
+        <li>
+          <NavLink to="countries">Countries</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default AppNav
+export default AppNav;
