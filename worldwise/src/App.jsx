446008a015c6ca5eb93b2b80cb71d91fbccdce51
younusraza909 <younusraza909@gmail.com>
2023-08-15 10:38:11 +0500

    Adding react-router-dom to add routes in our SPA

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='product' element={<Product />} />
+        <Route path='pricing' element={<Pricing />} />
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
