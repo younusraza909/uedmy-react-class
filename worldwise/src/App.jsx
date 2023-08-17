3f69ae19f299029f95fbc5da24aa9576cb6d6f72
younusraza909 <younusraza909@gmail.com>
2023-08-17 11:23:20 +0500

    Navigate Component

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
@@ -38,10 +38,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='app' element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList isLoading={loading} cities={cities} />}
-          />
+          <Route index element={<Navigate replace to='cities' />} />
           <Route
             path='cities'
             element={<CityList isLoading={loading} cities={cities} />}
