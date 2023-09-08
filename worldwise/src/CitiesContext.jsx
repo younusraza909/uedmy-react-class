5475aa2feddb79bd244e419d98e7192e5d566412
younusraza909 <younusraza909@gmail.com>
2023-09-08 10:43:06 +0500

    Get Single City Context

--- worldwise/src/CitiesContext.jsx
+++ worldwise/src/CitiesContext.jsx
@@ -8,6 +8,7 @@ const CitiesContext = createContext();
 const CitiesProvider = ({ children }) => {
   const [cities, setCities] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(() => {
     async function fetchCities() {
@@ -26,11 +27,27 @@ const CitiesProvider = ({ children }) => {
     fetchCities();
   }, []);
 
+  async function getCity(id) {
+    try {
+      setLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch (error) {
+      console.log(error);
+      alert('There was an error loading data');
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
       value={{
         cities,
         loading,
+        getCity,
+        currentCity,
       }}
     >
       {children}
--- worldwise/src/components/City.jsx
+++ worldwise/src/components/City.jsx
@@ -1,9 +1,67 @@
-import { useParams } from "react-router-dom";
+import styles from './City.module.css';
+import ButtonBack from './BackButton';
+import { useCities } from '../CitiesContext';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Spinner from './Spinner';
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat('en', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    weekday: 'long',
+  }).format(new Date(date));
 
 function City() {
+  const { getCity, currentCity, loading } = useCities();
   const { id } = useParams();
 
-  return <h1>City :{id}</h1>;
+  useEffect(() => {
+    getCity(id);
+  }, [id]);
+
+  const { cityName, emoji, date, notes } = currentCity;
+
+  if (loading) return <Spinner />;
+
+  return (
+    <div className={styles.city}>
+      <div className={styles.row}>
+        <h6>City name</h6>
+        <h3>
+          <span>{emoji}</span> {cityName}
+        </h3>
+      </div>
+
+      <div className={styles.row}>
+        <h6>You went to {cityName} on</h6>
+        <p>{formatDate(date || null)}</p>
+      </div>
+
+      {notes && (
+        <div className={styles.row}>
+          <h6>Your notes</h6>
+          <p>{notes}</p>
+        </div>
+      )}
+
+      <div className={styles.row}>
+        <h6>Learn more</h6>
+        <a
+          href={`https://en.wikipedia.org/wiki/${cityName}`}
+          target='_blank'
+          rel='noreferrer'
+        >
+          Check out {cityName} on Wikipedia &rarr;
+        </a>
+      </div>
+
+      <div>
+        <ButtonBack />
+      </div>
+    </div>
+  );
 }
 
 export default City;
