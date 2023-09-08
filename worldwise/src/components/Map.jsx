b233a4e3801ef7d35a70403ba8c84b83013987be
younusraza909 <younusraza909@gmail.com>
2023-09-08 11:03:49 +0500

    Integration Leaf let map

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -1,17 +1,25 @@
-import styles from "./Map.module.css";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import styles from './Map.module.css';
+
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { useState } from 'react';
 
 function Map() {
-  const navigate = useNavigate();
-  const [params, setParams] = useSearchParams();
-
-  let lat = params.get("lat");
-  let lng = params.get("lng");
+  const [mapPosition, setMapPosition] = useState([40, 0]);
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      {lat}, {lng}
-      MAP
+    <div className={styles.mapContainer}>
+      <MapContainer className={styles.map} center={mapPosition} zoom={13}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+      ,
     </div>
   );
 }
