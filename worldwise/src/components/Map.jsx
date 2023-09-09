1ba0a0a0327e5a7cd540a917563f1e40f04e52a7
younusraza909 <younusraza909@gmail.com>
2023-09-09 09:47:27 +0500

    Added Multiple marker in the map

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -2,8 +2,10 @@ import styles from './Map.module.css';
 
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import { useState } from 'react';
+import { useCities } from '../CitiesContext';
 
 function Map() {
+  const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
 
   return (
@@ -13,11 +15,16 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
       ,
     </div>
