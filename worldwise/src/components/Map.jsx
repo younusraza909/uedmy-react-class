d94fc64b5dc44089befd8103ed80c8734cdb8db6
younusraza909 <younusraza909@gmail.com>
2023-09-09 10:08:26 +0500

    Adding more functionality in out map component

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -1,16 +1,33 @@
+/* eslint-disable react/prop-types */
 import styles from './Map.module.css';
 
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
-import { useState } from 'react';
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvents,
+} from 'react-leaflet';
+import { useEffect, useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { useCities } from '../CitiesContext';
 
 function Map() {
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
 
+  const [searchParams, setSearchParams] = useSearchParams();
+  const mapLat = searchParams.get('lat');
+  const mapLng = searchParams.get('lng');
+
+  useEffect(() => {
+    if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+  }, [mapLat, mapLng]);
+
   return (
     <div className={styles.mapContainer}>
-      <MapContainer className={styles.map} center={mapPosition} zoom={13}>
+      <MapContainer className={styles.map} center={mapPosition} zoom={6}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
@@ -25,10 +42,30 @@ function Map() {
             </Popup>
           </Marker>
         ))}
+
+        <ChangeMapView position={mapPosition} />
+        <DetectClick />
       </MapContainer>
-      ,
     </div>
   );
+}
+
+// Leaf let provide these component and they just add more functionality to existing map
+
+function ChangeMapView({ position }) {
+  let map = useMap();
+  map.setView(position);
+
+  return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+
+  useMapEvents({
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  });
+  return null;
 }
 
 export default Map;
