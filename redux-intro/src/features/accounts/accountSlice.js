0457e9d04f64079b0971a204816c42b026dbe4fd
younusraza909 <younusraza909@gmail.com>
2023-10-24 10:41:41 +0500

    redux-intro : Using RTK for creating slice

--- redux-intro/src/features/accounts/accountSlice.js
+++ redux-intro/src/features/accounts/accountSlice.js
@@ -1,63 +1,53 @@
-const initialStateAccount = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
-  isLoading: false
+  isLoading: false,
 };
 
-export default function accountReducer(state = initialStateAccount, action) {
-  switch (action.type) {
-    case "account/deposit":
-      return { ...state, balance: state.balance + action.payload, isLoading: false };
-    case "account/withdraw":
-      return { ...state, balance: state.balance - action.payload };
-    case "account/requestLoan":
-      if (state.loan > 0) return state;
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.purpose,
-        balance: state.balance + action.payload.amount,
-      };
-    case "account/payLoan":
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: "",
-        balance: state.balance - state.loan,
-      };
-    case "account/convertingCurrency":
-      return {
-        ...state,
-        isLoading: true
-      }
+//This auto created action only get single arguments
+const accountSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    deposit(state, action) {
+      state.balance = state.balance + action.payload;
+    },
+    withdraw(state, action) {
+      state.balance = state.balance - action.payload;
+    },
+    // by default request loan will get only one argument but now how would I get by both arguments ?
+    // we can prepare our payload
+    requestLoan: {
+      prepare(amount, purpose) {
+        return {
+          payload: {
+            amount,
+            purpose,
+          },
+        };
+      },
+      reducer(state, action) {
+        if (state.loan > 0) return;
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance = state.balance + action.payload.amount;
+      },
+    },
+    payLoan(state, action) {
+      state.loanPurpose = "";
+      state.balance = state.balance - state.loan;
+      state.loan = 0;
+    },
+    convertingCurrency(state, action) {
+      state.isLoading = true;
+    },
+  },
+});
 
-    default:
-      return state;
-  }
-}
-export function deposit(amount, currency) {
-  if (currency === "USD") return { type: "account/deposit", payload: amount };
+export default accountSlice.reducer;
 
-  return async function (dispatch, getState) {
-    dispatch({ type: "account/convertingCurrency" })
-
-    const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`)
-    const data = await res.json()
-    const converted = data.rates.USD
-    // return action
-    dispatch({ type: "account/deposit", payload: converted })
-  }
-}
-
-export function withdraw(amount) {
-  return { type: "account/withdraw", payload: amount };
-}
-
-export function requestLoan(amount, purpose) {
-  return { type: "account/requestLoan", payload: { amount, purpose } };
-}
-
-export function payLoan() {
-  return { type: "account/payLoan" };
-}
+export const { deposit, withdraw, requestLoan, payLoan, convertingCurrency } =
+  accountSlice.actions;
