e913ae673857dcb80c7a43b1648b022c14e0d66e
younusraza909 <younusraza909@gmail.com>
2023-10-24 10:59:46 +0500

    redux-intro:Creating Customer Slice

--- redux-intro/src/features/customers/customerSlice.js
+++ redux-intro/src/features/customers/customerSlice.js
@@ -1,36 +1,38 @@
-const initialStateCustomer = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   fullName: "",
   nationalID: "",
   createdAt: "",
 };
 
-export default function customerReducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case "customer/createCustomer":
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        nationalID: action.payload.nationalID,
-        createdAt: action.payload.createdAt,
-      };
-    case "customer/updateName":
-      return { ...state, fullName: action.payload.fullName };
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
+  reducers: {
+    //Again we have to prepare data as it has only one argument supoort by defaul
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = action.payload.createdAt;
+      },
+    },
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
 
-    default:
-      return state;
-  }
-}
+export const { createCustomer, updateName } = customerSlice.actions;
 
-export function createCustomer(fullName, nationalID) {
-  return {
-    type: "customer/createCustomer",
-    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
-  };
-}
-
-export function updateName(fullName) {
-  return {
-    type: "customer/updateName",
-    payload: { fullName },
-  };
-}
+export default customerSlice.reducer;
