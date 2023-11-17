57529d5cdd7af88c45135b07b202bc046eec234a
younusraza909 <younusraza909@gmail.com>
2023-11-17 15:24:01 +0500

    fast-react-pizza : Making a app layout

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,25 +1,33 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import Home from './ui/Home';
-import Menu from './features/menu/Menu';
-import Cart from './features/cart/Cart';
-import CreateOrder from './features/order/CreateOrder';
-import Order from './features/order/Order';
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
 
 // in react-router@6 implementation is changed now you can fetch data and render
 // instead of render and fetch
+
+// here children means the app layout will be shown in every route because it is layout and than in that we are showing different route
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      { path: "/cart", element: <Cart /> },
+      { path: "/cart/new", element: <CreateOrder /> },
+      { path: "/order/:orderId", element: <Order /> },
+    ],
   },
-  {
-    path: '/menu',
-    element: <Menu />,
-  },
-  { path: '/cart', element: <Cart /> },
-  { path: '/cart/new', element: <CreateOrder /> },
-  { path: '/order/:orderId', element: <Order /> },
 ]);
 
 function App() {
---
+++ fast-react-pizza/src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <p>Younus</p>
+    </div>
+  );
+}
+
+export default Header;
