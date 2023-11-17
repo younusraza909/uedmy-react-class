163a7d8cfc12df53b20fafbb0308681b84ac3757
younusraza909 <younusraza909@gmail.com>
2023-11-17 15:48:58 +0500

    fast-react-pizza : adding a loader state for react loader function

--- fast-react-pizza/src/ui/AppLayout.jsx
+++ fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,11 +1,17 @@
 import Header from "./Header";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 
 import CartOverview from "../features/cart/CartOverview";
+import Loader from "./Loader";
 
 function AppLayout() {
+  const navigation = useNavigation();
+
+  const isLoading = navigation.state === "loading";
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
 
       <main>
