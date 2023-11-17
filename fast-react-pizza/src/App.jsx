3ffa7504e3eaaf91f006eb39682534ad951e2552
younusraza909 <younusraza909@gmail.com>
2023-11-17 12:06:01 +0500

    fast-react-app : react-router@6 new implementation for fetch-and-render

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import Home from './ui/Home';
+import Menu from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import CreateOrder from './features/order/CreateOrder';
+import Order from './features/order/Order';
+
+// in react-router@6 implementation is changed now you can fetch data and render
+// instead of render and fetch
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/menu',
+    element: <Menu />,
+  },
+  { path: '/cart', element: <Cart /> },
+  { path: '/cart/new', element: <CreateOrder /> },
+  { path: '/order/:orderId', element: <Order /> },
+]);
 
 function App() {
-  const x = 23;
-  return <div>Hello Vite</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
