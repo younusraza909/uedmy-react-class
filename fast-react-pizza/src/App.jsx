2b1dd29f70a72d6cb2d9d860e0b3753655c9d7b1
younusraza909 <younusraza909@gmail.com>
2023-11-17 15:37:49 +0500

    fast-react-pizza : using loader from react-router v6

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,7 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
@@ -22,6 +22,7 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
+        loader: menuLoader,
       },
       { path: "/cart", element: <Cart /> },
       { path: "/cart/new", element: <CreateOrder /> },
--- fast-react-pizza/src/features/menu/Menu.jsx
+++ fast-react-pizza/src/features/menu/Menu.jsx
@@ -1,5 +1,29 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+
+import MenuItem from "./MenuItem";
+
 function Menu() {
-  return <h1>Menu</h1>;
+  // React router knows that by using this hook we want data fetched by loader into this component
+  const menu = useLoaderData();
+
+  console.log("Menu 2", menu);
+
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
+}
+
+// Creating a loader so it can called by router in latest router v6
+export async function loader() {
+  const menu = await getMenu();
+
+  console.log("Menu", menu);
+  return menu;
 }
 
 export default Menu;
