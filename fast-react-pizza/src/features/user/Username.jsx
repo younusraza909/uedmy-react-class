5b917a64a2b0913b2975f1505d4e153b2d8497a6
younusraza909 <younusraza909@gmail.com>
2023-11-22 11:12:56 +0500

    fast-react-pizza: Working with tailwind box model

--- fast-react-pizza/src/features/user/Username.jsx
+++ fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Younus</div>;
+  return <div className="text-sm font-semibold hidden">Younus</div>;
 }
 
 export default Username;
