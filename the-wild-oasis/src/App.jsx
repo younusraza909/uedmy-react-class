7864bb16840eda981ecc58f792ed7df5f3e36e79
younusraza909 <younusraza909@gmail.com>
2023-12-04 13:46:05 +0500

    the-wild-oasis : Setting up react router dom

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,20 +1,28 @@
-import { styled } from "styled-components";
-import GlobalStyles from "./styles/GlobalStyles";
-import Heading from "./ui/Heading";
-
-const AppComponent = styled.div`
-  background-color: var(--color-brand-50);
-`;
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import Settings from "./pages/Settings";
+import Users from "./pages/Users";
 
 function App() {
-  // GlobalStyles cannot takes props or children so we need to put it as siblings to app
   return (
-    <>
-      <GlobalStyles />
-      <AppComponent>
-        <Heading as="h2">Hello World</Heading>
-      </AppComponent>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate replace to="dashboard" />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="bookings" element={<Bookings />} />
+        <Route path="cabins" element={<Cabins />} />
+        <Route path="users" element={<Users />} />
+        <Route path="settings" element={<Settings />} />
+        <Route path="account" element={<Account />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
