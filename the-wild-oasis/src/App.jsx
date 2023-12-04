d6270734263bb41c8407cf7eab6512b512fee65b
younusraza909 <younusraza909@gmail.com>
2023-12-04 12:06:07 +0500

    the-wild-oasis:making a stlye component for heading

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,11 +1,19 @@
+import { styled } from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
+import Heading from "./ui/Heading";
+
+const AppComponent = styled.div`
+  background-color: var(--color-brand-50);
+`;
 
 function App() {
   // GlobalStyles cannot takes props or children so we need to put it as siblings to app
   return (
     <>
-      <GlobalStyles></GlobalStyles>
-      <div>Hello Wold</div>
+      <GlobalStyles />
+      <AppComponent>
+        <Heading as="h2">Hello World</Heading>
+      </AppComponent>
     </>
   );
 }
