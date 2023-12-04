ceea97613081a4518f2ea7e77a517c5b90c3eced
younusraza909 <younusraza909@gmail.com>
2023-12-04 11:47:07 +0500

    the-wild-oasis : Adding Global Styles using createGlobalStyles function

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,5 +1,13 @@
+import GlobalStyles from "./styles/GlobalStyles";
+
 function App() {
-  return <div>Hello Wold</div>;
+  // GlobalStyles cannot takes props or children so we need to put it as siblings to app
+  return (
+    <>
+      <GlobalStyles></GlobalStyles>
+      <div>Hello Wold</div>
+    </>
+  );
 }
 
 export default App;
