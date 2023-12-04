ff780a342a02b316d6db1b3eb8fb8a497773aec5
younusraza909 <younusraza909@gmail.com>
2023-12-04 15:47:04 +0500

    the-wild-oasis : making side bar with Main Nav

--- the-wild-oasis/src/ui/Sidebar.jsx
+++ the-wild-oasis/src/ui/Sidebar.jsx
@@ -1,4 +1,6 @@
 import { styled } from "styled-components";
+import Logo from "./Logo";
+import MainNav from "./MainNav";
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
@@ -9,7 +11,12 @@ const StyledSidebar = styled.aside`
 `;
 
 function Sidebar() {
-  return <StyledSidebar>Sidebar</StyledSidebar>;
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
 }
 
 export default Sidebar;
