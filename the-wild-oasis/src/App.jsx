d0b37183450c892dc6c4a253df62237ceb8bc61b
younusraza909 <younusraza909@gmail.com>
2023-12-06 11:27:19 +0500

    the-wild-oasis : Fetching Cabins Data

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -17,7 +17,8 @@ import GlobalStyles from "./styles/GlobalStyles";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 60 * 1000, //1 min
+      // staleTime: 60 * 1000, //1 min
+      staleTime: 0,
     },
   },
 });
