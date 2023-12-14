1fbefbbcdd2248da1cef2f2cec2816a774c908e5
younusraza909 <younusraza909@gmail.com>
2023-12-14 11:33:03 +0500

    compund-component-pattern : Final Files

--- compound-component-pattern/src/App.js
+++ compound-component-pattern/src/App.js
@@ -4,14 +4,27 @@ export default function App() {
   return (
     <div>
       <h1>Compound Component Pattern</h1>
-      <Counter
+      {/* <Counter
         iconIncrease="+"
         iconDecrease="-"
         label="My NOT so flexible counter"
         hideLabel={false}
         hideIncrease={false}
         hideDecrease={false}
-      />
+      /> */}
+      <div>
+        <Counter>
+          <Counter.Label>Compund Component Pattern Counter</Counter.Label>
+          <Counter.Increase icon="+" />
+          <Counter.Value />
+          <Counter.Decrease icon="-" />
+        </Counter>
+      </div>
+      <Counter>
+        <Counter.Increase icon="+" />
+        <Counter.Value />
+        <Counter.Decrease icon="-" />
+      </Counter>
     </div>
   );
 }
--- compound-component-pattern/src/Counter.js
+++ compound-component-pattern/src/Counter.js
@@ -1,4 +1,50 @@
-function Counter() {
-  return <span>Counter</span>;
+import { createContext, useContext, useState } from "react";
+
+const CounterProvider = createContext();
+
+function Counter({ children }) {
+  const [count, setCount] = useState(0);
+
+  const increaseCounterHandler = () => {
+    setCount((count) => count + 1);
+  };
+
+  const decreaseCounterHandler = () => {
+    setCount((count) => count - 1);
+  };
+
+  return (
+    <CounterProvider.Provider
+      value={{ count, increaseCounterHandler, decreaseCounterHandler }}
+    >
+      {children}
+    </CounterProvider.Provider>
+  );
 }
+
+function Label({ children }) {
+  return <span>{children}</span>;
+}
+
+function Increase({ icon }) {
+  const { increaseCounterHandler } = useContext(CounterProvider);
+  return <button onClick={increaseCounterHandler}>{icon}</button>;
+}
+
+function Value() {
+  const { count } = useContext(CounterProvider);
+  return <span>{count}</span>;
+}
+
+function Decrease({ icon }) {
+  const { decreaseCounterHandler } = useContext(CounterProvider);
+  return <button onClick={decreaseCounterHandler}>{icon}</button>;
+}
+
+// Adding other component as property
+Counter.Label = Label;
+Counter.Increase = Increase;
+Counter.Value = Value;
+Counter.Decrease = Decrease;
+
 export default Counter;
