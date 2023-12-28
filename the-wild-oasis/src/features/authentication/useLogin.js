a6bdad97f312110207c2c34004dc428ed961a598
younusraza909 <younusraza909@gmail.com>
2023-12-28 10:41:25 +0500

    the-wild-oasis : The user login form

---
+++ the-wild-oasis/src/features/authentication/useLogin.js
@@ -0,0 +1,24 @@
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { login as loginApi } from "../../services/apiAuthentication";
+
+export default function useLogin() {
+  const navigate = useNavigate();
+
+  const { mutate: login, isLoading } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+    onSuccess: (data) => {
+      console.log("data", data);
+
+      toast.success("Successfully Logged in");
+      navigate("/dashboard");
+    },
+    onError: (err) => {
+      console.log("ERROR", err);
+      toast.error("Provided email or password are incorrect");
+    },
+  });
+
+  return { login, isLoading };
+}
---
+++ the-wild-oasis/src/services/apiAuthentication.js
@@ -0,0 +1,14 @@
+import supabase from "./supabase";
+
+export async function login({ email, password }) {
+  let { data, error } = await supabase.auth.signInWithPassword({
+    email: email,
+    password: password,
+  });
+
+  if (error) {
+    console.error(error);
+    throw new Error(error.message);
+  }
+  return data;
+}
