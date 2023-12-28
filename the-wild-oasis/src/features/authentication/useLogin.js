abd5b201a8c0cc6cf5d42077b5e425093213a019
Muhammad Younus Raza <younusraza909@gmail.com>
2023-12-28 21:11:55 +0500

    the-wild-oasis : Important Bug Solved

--- the-wild-oasis/src/features/authentication/useLogin.js
+++ the-wild-oasis/src/features/authentication/useLogin.js
@@ -1,16 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { login as loginApi } from "../../services/apiAuthentication";
 
 export default function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient()
 
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (data) => {
-      console.log("data", data);
 
+      queryClient.setQueryData(['user'], data.user)
       toast.success("Successfully Logged in");
       navigate("/dashboard");
     },
--- the-wild-oasis/src/ui/ProtectRoute.jsx
+++ the-wild-oasis/src/ui/ProtectRoute.jsx
@@ -1,8 +1,8 @@
-import styled from "styled-components";
-import useUser from "../features/authentication/useUser";
-import Spinner from "./Spinner";
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import styled from 'styled-components';
+import useUser from '../features/authentication/useUser';
+import Spinner from './Spinner';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const FullPage = styled.div`
   height: 100vh;
@@ -18,12 +18,10 @@ function ProtectRoute({ children }) {
   // 1. Load the authenticated user
   const { isLoading, isAuthenticated } = useUser();
 
-  console.log("isLoading", isLoading, isAuthenticated);
-
   // 2. If there is NO authenticated user, redirect to the /login
   useEffect(
     function () {
-      if (!isAuthenticated && !isLoading) navigate("/login");
+      if (!isAuthenticated && !isLoading) navigate('/login');
     },
     [isAuthenticated, isLoading, navigate]
   );
