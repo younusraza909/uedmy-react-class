dcc93615fc48d59089ef6352b3e9dbde1fec7684
Muhammad Younus Raza <younusraza909@gmail.com>
2023-12-30 21:28:09 +0500

    the-wild-oasis : Displaying Stays for current day

--- the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import { useRecentStays } from './useRecentStays';
 import Stats from './Stats';
 import SalesChart from './SalesChart';
 import DurationChart from './DurationChart';
+import TodayActivity from '../check-in-out/TodayActivity';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -37,9 +38,7 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-      <div>
-        <p>Chart 2</p>
-      </div>
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
