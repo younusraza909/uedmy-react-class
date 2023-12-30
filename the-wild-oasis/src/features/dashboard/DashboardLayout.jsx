486060809ab6dfeb13a8389b15196a78f30b20d5
Muhammad Younus Raza <younusraza909@gmail.com>
2023-12-30 20:20:42 +0500

    the-wild-oasis : Fetch recent bookings and stays

--- the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import Spinner from '../../ui/Spinner';
+import { useRecentBookings } from './useRecentBookings';
+import { useRecentStays } from './useRecentStays';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,32 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { isLoading: isLoading1, bookings } = useRecentBookings();
+  console.log('isLoading1', isLoading1, bookings);
+
+  const { isLoading: isLoading2, stays } = useRecentStays();
+  console.log('isLoading2', isLoading2, stays);
+
+  if (isLoading1 || isLoading2) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <div>
+        <p>Chart 1</p>
+      </div>
+      <div>
+        <p>Chart 2</p>
+      </div>
+      <div>
+        <p>Chart 3</p>
+      </div>
+      <div>
+        <p>Chart 4</p>
+      </div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
---
+++ the-wild-oasis/src/features/dashboard/useRecentStays.js
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import { getStaysAfterDate } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
+import { subDays } from "date-fns";
+
+
+export function useRecentStays() {
+    const [searchParams] = useSearchParams()
+
+
+    const numDays = !searchParams.get("last") ? 7 : Number(searchParams.get('last'))
+
+    const queryDate = subDays(new Date(), numDays).toISOString()
+
+    const { data: stays, isLoading } = useQuery({
+        queryFn: () =>
+            getStaysAfterDate(queryDate),
+        queryKey: ['stays', `last-${numDays}`]
+    });
+
+    return { stays, isLoading };
+}
